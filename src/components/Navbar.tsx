f85809dc75b512808e4c9f5ac4a296b3ed959a1d
Jakub Cabala <kuba.cabala6@gmail.com>
2024-05-28 12:24:04 +0100

    FEAT: Added a navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState } from "react";
+import config from "@/data/navbarConfig";
+
+export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-orange-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href="#pablo"
+            >
+              {config.title}
+            </a>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              {config.links.map((link) => (
+                <li className="nav-item" key={link.title}>
+                  <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    href={link.path}
+                  >
+                    <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+                    <span className="ml-2">{link.title}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
---
+++ src/data/navbarConfig.js
@@ -0,0 +1,23 @@
+const navbarConfig = {
+  title: "Fill that gap",
+  links: [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ],
+};
+
+export default navbarConfig;
