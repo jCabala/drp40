dfcbf765ef277e95b80dd5c7733c447c26c772a7
Nicolas Schleicher <ns2622@ic.ac.uk>
2024-06-03 14:45:41 +0100

    FEAT: Install image slider library and add columns to see mroe page

--- src/app/flat/[id]/page.tsx
+++ src/app/flat/[id]/page.tsx
@@ -1,13 +1,11 @@
-import Card from "@/components/Card";
-import DetailedCard from "@/components/DetailedFlat";
+"use client"
+import RoomView from "@/components/RoomView"
+import SeeMoreMainViews from "@/components/SeeMoreMainView"
 
 export default function Page({ params }: { params: { id: string } }) {
-    return DetailedCard({
-        id: 1,
-        rentPerWeek: 100,
-        numberOfRooms: 3,
-        numberOfGaps: 2,
-        img1: "https://www.srijanrealty.com/wp-content/uploads/2022/07/3-bhk-flat-in-kolkata.png",
-        img2: "https://media.gettyimages.com/id/1307394117/photo/female-and-male-friends-having-breakfast-in-living-room-at-home.jpg?s=612x612&w=gi&k=20&c=jgGvUnK92GXx_mxIA6X3-9bA4Y0iSBtd8_MIS5-yWRQ="
-      });
+    return (
+        <div className="w-full flex flex-row">
+        <SeeMoreMainViews id={params.id}/>
+      </div>
+    )
   }
---
+++ src/components/RoomView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+
+type Props = {
+    original: string,
+    thumbnail: string
+  };
+
+function RoomView(images: Props[]) {
+    return <ImageGallery items={images} showPlayButton={false} thumbnailPosition="right"/>;
+}
+
+export default RoomView;
