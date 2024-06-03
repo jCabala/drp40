91bd51f22fc86c39bbbc001ff22debdf9b96bf53
Jakub Cabala <kuba.cabala6@gmail.com>
2024-06-03 13:12:18 +0100

    FEAT: Added logic for uploading images

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { db, storage } from "@/lib/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 function Form() {
-  return <div>Form</div>;
+  const rentRef = useRef<HTMLInputElement>(null);
+  const roomsRef = useRef<HTMLInputElement>(null);
+  const gapsRef = useRef<HTMLInputElement>(null);
+  const imgRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Upload images
+    const images = imgRef.current?.files;
+    if (!images || images?.length == 0) {
+      alert("Upload some images!");
+      return;
+    }
+
+    for (let idx = 0; idx < images.length; idx++) {
+      const img = images[idx];
+      const storageRef = ref(storage, `flatImages/${img.name}-${Date.now()}`);
+      const uploadTask = uploadBytes(storageRef, img);
+
+      const url = await getDownloadURL((await uploadTask).ref);
+      console.log(url);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="number" ref={rentRef} placeholder="Rent per week" required />
+      <input
+        type="number"
+        ref={roomsRef}
+        placeholder="Number of rooms"
+        required
+      />
+      <input
+        type="number"
+        ref={gapsRef}
+        placeholder="Number of gaps"
+        required
+      />
+      <input
+        type="file"
+        ref={imgRef}
+        multiple
+        accept="image/png, image/gif, image/jpeg, image/jpg, image/webp"
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
 
 export default Form;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-export { db };
+const storage = getStorage(app);
+export { db, storage };
