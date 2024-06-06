f24130d851e0b2edbfb69c33ae756b530ea22a0c
Jakub Cabala <kuba.cabala6@gmail.com>
2024-06-06 13:53:08 +0100

    FEAT: Added a alert component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Label from "./Label";
+import { useRouter } from "next/router";
 
 type Props = {
   id: string;
@@ -22,59 +23,63 @@ function Card({
   numberOfGaps,
   labels,
 }: Props) {
-  return (
-    <div className="mb-10 overflow-hidden rounded-lg border-2 border-orange-500 shadow-5 duration-300 hover:scale-105 flex flex-row min-h-60 w-full">
-      <div
-        style={
-          img2
-            ? {
-                background: `url(${img2})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }
-            : {}
-        }
-        className="bg-center bg-black w-1/2"
-      >
-        <img
-          src={img1}
-          alt=""
-          className={`size-full object-cover ${
-            img2 ? "opacity-100 hover:opacity-0 duration-300" : ""
-          }`}
-        />
-      </div>
-      <div className="h-full min-h-60 p-2 w-1/2 flex flex-col justify-between">
-        <div>
-          <p className="mx-1">
-            <b className="text-orange-500">Rent: </b>
-            {rentPerWeek} £/week
-          </p>
-          <p className="mx-1">
-            <b className="text-orange-500">Campus travel: </b>30 min
-          </p>
-          <p className="mx-1">
-            <b className="text-orange-500">Number of rooms: </b>
-            {numberOfRooms}
-          </p>
-          <p className="mx-1">
-            Looking for <b className="text-orange-500">{numberOfGaps}</b>{" "}
-            tenants
-          </p>
-          <div className="flex flex-wrap pt-3">
-            {labels?.map((label) => (
-              <Label key={label.name} name={label.name} color={label.color} />
-            ))}{" "}
-          </div>
-        </div>
+  const flatPath = `/flat/${id}`;
 
-        <Link className="self-end" href={`/flat/${id}`}>
+  return (
+    <Link className="self-end" href={flatPath}>
+      <div
+        className="mb-10 overflow-hidden rounded-lg border-2 border-orange-500 shadow-5 duration-300 hover:scale-105 flex flex-row min-h-60 w-full"
+      >
+        <div
+          style={
+            img2
+              ? {
+                  background: `url(${img2})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }
+              : {}
+          }
+          className="bg-center bg-black w-1/2"
+        >
+          <img
+            src={img1}
+            alt=""
+            className={`size-full object-cover ${
+              img2 ? "opacity-100 hover:opacity-0 duration-300" : ""
+            }`}
+          />
+        </div>
+        <div className="h-full min-h-60 p-2 w-1/2 flex flex-col justify-between">
+          <div>
+            <p className="mx-1">
+              <b className="text-orange-500">Rent: </b>
+              {rentPerWeek} £/week
+            </p>
+            <p className="mx-1">
+              <b className="text-orange-500">Campus travel: </b>30 min
+            </p>
+            <p className="mx-1">
+              <b className="text-orange-500">Number of rooms: </b>
+              {numberOfRooms}
+            </p>
+            <p className="mx-1">
+              Looking for <b className="text-orange-500">{numberOfGaps}</b>{" "}
+              tenants
+            </p>
+            <div className="flex flex-wrap pt-3">
+              {labels?.map((label) => (
+                <Label key={label.name} name={label.name} color={label.color} />
+              ))}{" "}
+            </div>
+          </div>
+
           <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
             See more
           </button>
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
