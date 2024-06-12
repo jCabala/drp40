0f2818fde5286183857ae6ba2aecc8d396af925a
Elliot Benson <eb522@ic.ac.uk>
2024-06-12 13:16:54 +0100

    FEAT: Moved application loading up to page level

--- src/app/my-applications/page.tsx
+++ src/app/my-applications/page.tsx
@@ -1,9 +1,55 @@
 "use client";
+import LoadingOverlay from "@/components/helper/LoadingOverlay";
 import UserApplicationsView from "@/components/views/UserApplicationsView";
-import React from "react";
+import { FlatAdvertisment } from "@/data/flatAdvertisments";
+import { UserApplication } from "@/data/userApplication";
+import { UserProfile } from "@/data/userProfile";
+import { fetchAllApplicationsByUserID, fetchFlatByID } from "@/lib/firebase";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
 
 function MyApplications() {
-  return <UserApplicationsView />;
+  const [applicationsWithFlat, setApplicationsWithFlat] =
+    useState<(UserApplication & { flat: FlatAdvertisment })[]>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const userID = Cookies.get("userID");
+
+  const getApplications = async () => {
+    setIsLoading(true);
+    if (userID) {
+      const apps = await fetchAllApplicationsByUserID(userID);
+      const flats = (
+        await Promise.all(
+          apps.map(async (app) => {
+            return await fetchFlatByID(app.flatID);
+          })
+        )
+      ).filter((flat): flat is FlatAdvertisment => flat !== null);
+
+      const myApplicationsWithFlat = apps.map((app, index) => ({
+        ...app,
+        flat: flats[index],
+      }));
+      setApplicationsWithFlat(myApplicationsWithFlat);
+
+      setTimeout(() => setIsLoading(false), 600);
+    }
+  };
+
+  useEffect(() => {
+    getApplications();
+  }, [userID]);
+
+  return (
+    <>
+      {isLoading && <LoadingOverlay />}
+      <UserApplicationsView
+        applications={applicationsWithFlat || []}
+        getApplications={getApplications}
+      />
+    </>
+  );
 }
 
 export default MyApplications;
--- src/components/views/UserApplicationsView.tsx
+++ src/components/views/UserApplicationsView.tsx
@@ -1,44 +1,20 @@
-import {
-  fetchAllApplicationsByUserID,
-  fetchFlat,
-  fetchFlatByID,
-} from "@/lib/firebase";
-import React, { useState } from "react";
+import { fetchAllApplicationsByUserID, fetchFlatByID } from "@/lib/firebase";
+import React, { useState, useEffect } from "react";
 import MyApplicationCard from "../cards/MyApplicationCard";
 import Cookies from "js-cookie";
 import { UserApplication } from "@/data/userApplication";
 import { FlatAdvertisment } from "@/data/flatAdvertisments";
 
-function UserApplicationsView() {
-  const [applicationsWithFlat, setApplicationsWithFlat] =
-    useState<(UserApplication & { flat: FlatAdvertisment })[]>();
+type Props = {
+  applications: (UserApplication & { flat: FlatAdvertisment })[];
+  getApplications: () => void;
+};
 
-  const userID = Cookies.get("userID");
-  const fetchData = async () => {
-    if (userID) {
-      const apps = await fetchAllApplicationsByUserID(userID);
-      const flats = (
-        await Promise.all(
-          apps.map(async (app) => {
-            return await fetchFlatByID(app.flatID);
-          })
-        )
-      ).filter((flat): flat is FlatAdvertisment => flat !== null);
-
-      const myApplicationsWithFlat = apps.map((app, index) => ({
-        ...app,
-        flat: flats[index],
-      }));
-      setApplicationsWithFlat(myApplicationsWithFlat);
-    }
-  };
-
-  fetchData();
-
+function UserApplicationsView({ applications, getApplications }: Props) {
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      {applicationsWithFlat && applicationsWithFlat.length > 0 ? (
-        applicationsWithFlat.map((flatObj, idx) => (
+      {applications && applications.length > 0 ? (
+        applications.map((flatObj, idx) => (
           <MyApplicationCard
             key={idx}
             img1={flatObj.flat.images[0]}
